d1cda04f1cb6e039537039c4633f6f5227322129
Jeff Korzeniowski <jeff.korzeniowski@gmail.com>
2022-09-05 14:35:57 -0400

    Initial Commit

---
+++ Offline/OfflineApp.swift
@@ -0,0 +1,17 @@
+//
+//  OfflineApp.swift
+//  Offline
+//
+//  Created by Jeff Korzeniowski on 9/5/22.
+//
+
+import SwiftUI
+
+@main
+struct OfflineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
