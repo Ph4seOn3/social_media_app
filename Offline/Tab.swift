a677d6ccaa6d341a5235590de5e277d9215da307
Jeff Korzeniowski <jeff.korzeniowski@gmail.com>
2022-09-22 13:37:59 -0400

    Made Refactoring changes to divide homepage body into multiple views.

--- Offline/Tab.swift
+++ Offline/Tab.swift
@@ -24,3 +24,10 @@ enum Tab: String {
     case notifications
     case library
 }
+
+struct TabPreferenceKey: PreferenceKey {
+    static var defaultValue: CGFloat = 0
+    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+        value = nextValue()
+    }
+}
