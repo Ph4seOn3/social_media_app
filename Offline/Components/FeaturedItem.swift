966c2ea8fcd359481351c02655107162d97bf60b
Jeff Korzeniowski <jeff.korzeniowski@gmail.com>
2022-09-26 10:30:49 -0400

    Horizontal Scroll Main Page Nav. Animation, paralax, blur, etc..

--- Offline/Components/FeaturedItem.swift
+++ Offline/Components/FeaturedItem.swift
@@ -8,10 +8,11 @@
 import SwiftUI
 
 struct FeaturedItem: View {
+    var course: Course = courses[0]
     var body: some View {
         VStack(alignment: .leading, spacing: 8.0) {
             Spacer()
-            Image("Logo 2")
+            Image(course.logo)
                 .resizable(resizingMode: . stretch)
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 26.0, height: 26.0)
@@ -20,15 +21,15 @@ struct FeaturedItem: View {
                 .background(.ultraThinMaterial, in:
                                 RoundedRectangle(cornerRadius: 16, style: .continuous))
                 .strokeStyle(cornerRadius: 16)
-            Text("Homepage")
+            Text(course.title)
                 .font(.largeTitle)
                 .fontWeight(.bold)
                 .foregroundStyle(.linearGradient(colors: [.primary, .primary.opacity(0.5)], startPoint: .topLeading, endPoint: .bottomTrailing))
-            Text("Subheading".uppercased())
+            Text(course.subtitle.uppercased())
                 .font(.footnote)
                 .fontWeight(.semibold)
                 .foregroundStyle(.secondary)
-            Text("paragraph text would be here and this would be the smallest of the three")
+            Text(course.text)
                 .font(.footnote)
                 .multilineTextAlignment(.leading)
                 .lineLimit(2)
@@ -39,21 +40,10 @@ struct FeaturedItem: View {
         .padding(.all, 20.0)
         .padding(.vertical, 20)
         .frame(height: 350.0)
-        .background(.ultraThinMaterial, in:
-                        RoundedRectangle(cornerRadius: 30, style: .continuous))
-        .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y:10)
+        .background(.ultraThinMaterial)
+        .mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
         .strokeStyle()
         .padding(.horizontal, 20)
-        .background(Image("Blob 1")
-            .offset(x: 250, y:-100)
-        )
-        .overlay(
-            Image("Illustration 5")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(height: 230)
-                .offset(x:32, y: -80)
-    )
     }
 }
 
