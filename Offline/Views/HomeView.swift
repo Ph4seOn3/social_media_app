ae11a9a5b81e2ceb08a32b24593f29fdecb7cf86
Jeff Korzeniowski <jeff.korzeniowski@gmail.com>
2022-09-27 11:42:45 -0400

    Modal Sheet in a Loop as well as the navigation not breaking when accessing Modals.

--- Offline/Views/HomeView.swift
+++ Offline/Views/HomeView.swift
@@ -8,6 +8,8 @@ struct HomeView: View {
     @State var show = false
     @State var showStatusBar = true
     @State var selectedID = UUID()
+    @State var showCourse = false
+    @State var selectedIndex = 0
     @EnvironmentObject var model: Model
 
     
@@ -88,7 +90,7 @@ struct HomeView: View {
     
     var featured: some View {
         TabView {
-            ForEach(featuredCourses) { course in
+            ForEach(Array(featuredCourses.enumerated()), id: \.offset) { index, course in
                 GeometryReader { proxy in
                     let minX = proxy.frame(in: .global).minX
                     
@@ -107,18 +109,23 @@ struct HomeView: View {
                                 .offset(x:32, y: -80)
                                 .offset(x: minX / 2)
                         )
-
+                        .onTapGesture {
+                            showCourse = true
+                            selectedIndex = index
+                        }
                     
 //                    Text("\(proxy.frame(in: .global).minX)")
                 }
             }
-            
         }
         .tabViewStyle(.page(indexDisplayMode: .never))
         .frame(height:430)
         .background(Image("Blob 1")
             .offset(x: 250, y:-100)
         )
+        .sheet(isPresented: $showCourse) {
+            CourseView(namespace: namespace, course: featuredCourses[selectedIndex], show: $showCourse)
+        }
     }
     var cards: some View {
         ForEach(courses) { course in
