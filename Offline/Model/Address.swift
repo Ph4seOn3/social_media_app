183312412232af8d8227463976ece31da8438706
Jeff Korzeniowski <jeff.korzeniowski@gmail.com>
2022-09-28 15:54:26 -0400

    Dummy Location data api established for profile view

---
+++ Offline/Model/Address.swift
@@ -0,0 +1,13 @@
+//
+//  Address.swift
+//  Offline
+//
+//  Created by Jeff Korzeniowski on 9/28/22.
+//
+
+import SwiftUI
+
+struct Address: Identifiable, Decodable {
+    var id: Int
+    var country: String
+}
